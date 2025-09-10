d2daffcda83d72b4eb355cd48d5fb6ae77171dd8
PalomaFrea <pfrea@udesa.edu.ar>
2025-09-10 16:49:33 -0300

    .

--- trabajo-integrador/src/App.js
+++ trabajo-integrador/src/App.js
@@ -1,8 +1,11 @@
-
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route />
+      </Switch>
      
     </div>
   );
